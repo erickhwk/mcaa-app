2fba787953fe7c60985138e8ddf658c06f2b0981
Erick Valente <erickhwk@gmail.com>
2024-10-19 12:43:23 -0300

    add tailwind

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,3 +2,7 @@
 
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+pin "flowbite", to: "https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.turbo.min.js"
+pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
+pin "flowbite-datepicker", to: "https://cdn.jsdelivr.net/npm/flowbite@2.3.0/dist/datepicker.turbo.min.js"
+
